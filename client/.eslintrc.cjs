c0c191ab2922c46ddba78071ae578cfa6ec53692
Матвей Клёнов <y0na24@mail.ru>
2023-11-15 21:36:48 +0300

    fix eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -15,6 +15,8 @@ module.exports = {
 			'warn',
 			{ allowConstantExport: true },
 		],
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-unused-vars": "off",
 		'@typescript-eslint/ban-types': [
 			'error',
 			{
