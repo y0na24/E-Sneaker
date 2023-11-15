d9bd621ace0a1c1f43c6092136bf76102636a98a
y0na24 <110357779+y0na24@users.noreply.github.com>
2023-11-15 22:54:22 +0300

    cors

--- server/ecomprj/ecomprj/settings.py
+++ server/ecomprj/ecomprj/settings.py
@@ -58,9 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-
-    
+    'core.middleware.corsMiddleware',
 ]
 
 ROOT_URLCONF = 'ecomprj.urls'
