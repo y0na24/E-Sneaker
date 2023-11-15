af819bca3b0d80160a45e3a765d733adb4fff4fa
y0na24 <110357779+y0na24@users.noreply.github.com>
2023-11-15 22:31:51 +0300

    Update settings.py

--- server/ecomprj/ecomprj/settings.py
+++ server/ecomprj/ecomprj/settings.py
@@ -157,9 +157,8 @@ REST_FRAMEWORK = {
     )
 }
 
-CORS_ORIGIN_WHITELIST = [
+CORS_ALLOWED_ORIGINS = [
     'https://e-library-lac.vercel.app/',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
